11c60adabf77196b594bc6dce7e00f7c797aca5b
Ramona T <ramona.t.me@pm.me>
2026-07-30 10:48:46 -0400

    Add ratelimited APIConnector wrapper

---
+++ parsons/utilities/ratelimited_api_connector.py
@@ -0,0 +1,84 @@
+from typing import Any, Literal, overload
+
+import requests
+from pyrate_limiter import Limiter, Rate
+
+from parsons.utilities.api_connector import APIConnector
+
+
+class RateLimitedAPIConnector(APIConnector):
+    """A wrapper around APIConnector that adds rate limiting using pyrate-limiter."""
+
+    def __init__(self, *args, ratelimit: Rate, **kwargs) -> None:
+        self.limiter = Limiter(ratelimit)
+        super().__init__(*args, **kwargs)
+
+    def request(
+        self,
+        *args,
+        **kwargs,
+    ) -> requests.Response:
+        self.limiter.try_acquire("api_call")
+        return super().request(*args, **kwargs)
+
+    @overload
+    def get_request(
+        self,
+        url: ...,
+        *,
+        params: ... = ...,
+        return_format: Literal["json"] = "json",
+        raise_on_error: ... = ...,
+        **kwargs,
+    ) -> dict[str, Any]: ...
+
+    @overload
+    def get_request(
+        self,
+        url: ...,
+        *,
+        params: ... = ...,
+        return_format: Literal["content"],
+        raise_on_error: ... = ...,
+        **kwargs,
+    ) -> bytes: ...
+
+    def get_request(
+        self,
+        *args,
+        **kwargs,
+    ) -> dict[str, Any] | bytes:
+        self.limiter.try_acquire("api_call")
+        return super().get_request(*args, **kwargs)
+
+    def post_request(
+        self,
+        *args,
+        **kwargs,
+    ) -> dict[str, Any] | int | None:
+        self.limiter.try_acquire("api_call")
+        return super().post_request(*args, **kwargs)
+
+    def delete_request(
+        self,
+        *args,
+        **kwargs,
+    ) -> dict[str, Any] | int | None:
+        self.limiter.try_acquire("api_call")
+        return super().delete_request(*args, **kwargs)
+
+    def put_request(
+        self,
+        *args,
+        **kwargs,
+    ) -> dict[str, Any] | int | None:
+        self.limiter.try_acquire("api_call")
+        return super().put_request(*args, **kwargs)
+
+    def patch_request(
+        self,
+        *args,
+        **kwargs,
+    ) -> dict[str, Any] | int | None:
+        self.limiter.try_acquire("api_call")
+        return super().patch_request(*args, **kwargs)
